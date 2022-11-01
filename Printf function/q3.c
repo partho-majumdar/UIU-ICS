ac7443c91b6d6a9992012c6db2dd70c1b7834ac2
heipartho05 <heipartho72@gmail.com>
2022-11-01 19:52:24 +0600

    q3 added in printf function

---
+++ Printf function/q3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Myself Partho Majumdar. Life doesn\'t inspire me at all. It\'s meaningless. I know this and this is my key but I want to be a data scientist.");
+}
