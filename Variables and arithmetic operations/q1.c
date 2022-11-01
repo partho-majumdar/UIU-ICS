3341b166e9e33b38aaeafb041a3b97dad9035361
heipartho05 <heipartho72@gmail.com>
2022-11-01 19:58:48 +0600

    q1 added in variables and arithmetic operations

---
+++ Variables and arithmetic operations/q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int num1 = 17;
+    float num2 = 3.508;
+    char ch = 'W';
+
+    printf("Integer value is %d \n", num1);
+    printf("Floating point value is %f \n", num2);
+    printf("Character value is %c", ch);
+}
