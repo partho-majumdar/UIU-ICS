aa30c49fa863f7c3ace14f2a11015b364e7e38b5
heipartho05 <heipartho72@gmail.com>
2022-11-01 20:50:45 +0600

    q6 added in scanf function

---
+++ Scanf function/q6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    float radius, result;
+    float pi = 3.14159;
+
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+    result = pi * radius * radius;
+    printf("Area of a circle is: %.5f", result);
+}
+
+// area of a circle = pi * r * r
