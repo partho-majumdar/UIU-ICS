ba01bd22c7d9ddfe49611d088855399b4f1db0f3
heipartho05 <heipartho72@gmail.com>
2022-11-01 20:03:38 +0600

    q2 added in variables and arithmetic operations

---
+++ Variables and arithmetic operations/q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 18, b = 7;
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("multiplication = %d\n", a * b);
+    printf("Division = %d\n", a / b);
+    printf("remainder = %d\n", a % b);
+}
