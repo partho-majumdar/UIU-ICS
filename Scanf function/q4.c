525781ccfcb7ee9fe5ad52be41f3c0019bf613dc
heipartho05 <heipartho72@gmail.com>
2022-11-03 09:23:07 +0600

    add correct answer in scanf function for q4

--- Scanf function/q4.c
+++ Scanf function/q4.c
@@ -5,7 +5,8 @@ void main()
     int a, b, c, d, result;
 
     printf("Enter four number to calculate: ");
-    scanf("%d %d %d %d %d", &a, &b, &c, &d);
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
     result = a * b + (a - c) / d + b;
     printf("Answer: %d", result);
 }
