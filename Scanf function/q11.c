7b1be5c66436426fba4c5062494dc299037fc148
heipartho05 <heipartho72@gmail.com>
2022-11-03 10:39:24 +0600

    do nothing

--- Scanf function/q11.c
+++ Scanf function/q11.c
@@ -2,12 +2,12 @@
 
 void main()
 {
-    int input_val, hour, minute, second;
+    int input_value, hour, minute, second;
     printf("Enter any second: ");
-    scanf("%d", &input_val);
+    scanf("%d", &input_value);
 
-    hour = input_val / 3600;   // 525 / 3600 =-> 0
-    second = input_val % 3600; // 525 % 3600 = 525
+    hour = input_value / 3600;   // 525 / 3600 =-> 0
+    second = input_value % 3600; // 525 % 3600 = 525
 
     minute = second / 60; // 525 / 60 =-> 8
 
