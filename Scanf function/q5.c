4ffcb58942f5ff44e0145a3cf0d96c02e7b87c9b
heipartho05 <heipartho72@gmail.com>
2022-11-03 09:25:36 +0600

    add correct answer in scanf function for q5

--- Scanf function/q5.c
+++ Scanf function/q5.c
@@ -5,7 +5,8 @@ void main()
     float a, b, c, d, result;
 
     printf("Enter four number to calculate: ");
-    scanf("%f %f %f %f %f", &a, &b, &c, &d);
+    scanf("%f %f %f %f", &a, &b, &c, &d);
+
     result = (a + b - c) * d - a / d;
     printf("Answer: %.6f", result);
 }
