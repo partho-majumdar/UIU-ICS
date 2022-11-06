ee933c38fb991c30f2b39a624ab8f988253ec5a4
heipartho05 <heipartho72@gmail.com>
2022-11-06 23:10:05 +0600

    add math func in var and algo q4.c

--- Variables and arithmetic operations/q4.c
+++ Variables and arithmetic operations/q4.c
@@ -6,5 +6,9 @@ void main()
     float a = 21, b = 15, c = 34, d = 6, result;
 
     result = a * b + (a - c) / d + b;
+
+    // int only_int = floor(result);
+
     printf("Answer %d", (int)result);
+    // printf("Answer %d", only_int);
 }
