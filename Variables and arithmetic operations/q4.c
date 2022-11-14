4f7cc74e6a7e6bd4cd3ffb5ceb14abd4ba0f69eb
heipartho05 <heipartho72@gmail.com>
2022-11-14 22:31:44 +0600

    q4 update

--- Variables and arithmetic operations/q4.c
+++ Variables and arithmetic operations/q4.c
@@ -4,12 +4,18 @@
 void main()
 {
     // float a = 21, b = 15, c = 34, d = 6, result;
-    int result, a = 21, b = 15, c = 34, d = 6;
+    // int result, a = 21, b = 15, c = 34, d = 6;
 
-    result = a * b + (a - c) / d + b;
+    // result = a * b + (a - c) / d + b;
 
     // int final_ans = (result) - 1;
 
-    printf("Answer %d", result);
+    // printf("Answer %d", result);
     // printf("Answer %d", final_ans);
+
+    int result, a = 21, b = 15, c = 34, d = 6;
+
+    result = (float)a * b + (float)(a - c) / d + (float)b;
+
+    printf("Answer: %d", result);
 }
