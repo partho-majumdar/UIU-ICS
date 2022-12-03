a9971c1b0e810644372ba8010e677b6490b67434
heipartho05 <heipartho72@gmail.com>
2022-12-03 19:04:26 +0600

    q2 added

---
+++ Part-02/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter any number to check whether a number is even or odd: ");
+    scanf("%d", &input_value);
+
+    if (input_value % 2 == 0)
+    {
+        printf("You entered %d and this number is even", input_value);
+    }
+    else
+    {
+        printf("You entered %d and this number is odd", input_value);
+    }
+}
