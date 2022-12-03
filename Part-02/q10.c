dd440827f1ffaaf48477a6a84060ca554dacdc3d
heipartho05 <heipartho72@gmail.com>
2022-12-03 19:28:34 +0600

    q10 added

---
+++ Part-02/q10.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void main()
+{
+    float number1, number2;
+    char operator;
+
+    printf("Enter any operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter first number: ");
+    scanf("%f", &number1);
+
+    printf("Enter second number: ");
+    scanf("%f", &number2);
+
+    if (operator== '+')
+    {
+        printf("Sum of two number is: %f", number1 + number2);
+    }
+
+    else if (operator== '-')
+    {
+        printf("Difference of two number is: %f", number1 - number2);
+    }
+
+    else if (operator== '*')
+    {
+        printf("Multiplication of two number is: %f", number1 * number2);
+    }
+    else if (operator== '/')
+    {
+        if (number2 == 0)
+        {
+            printf("Zero as divisor is not valid!");
+        }
+        else
+        {
+            printf("Division of two number is: %f", number1 / number2);
+        }
+    }
+}
