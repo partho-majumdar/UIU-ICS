ba4d6b59167c14b560067ca699e0940d5bdd5a9b
heipartho05 <heipartho72@gmail.com>
2022-12-04 23:13:44 +0600

    q9 added

---
+++ Part-02/Solution/q9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+    char input_value;
+    printf("Enter any character: ");
+    scanf("%c", &input_value);
+
+    if (input_value >= 'a' && input_value <= 'z' || input_value >= 'A' && input_value <= 'Z' )
+    {
+        printf("'%c' is alphabet", input_value);
+    }
+
+    else if (input_value >= '0' && input_value <= '9')
+    {
+        printf("'%c' is digit", input_value);
+    }
+    else
+    {
+        printf("'%c' is special character");
+    }
+}
