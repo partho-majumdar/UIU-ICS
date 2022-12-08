0c30dce147b9f59c241821d5f9118ac0e47a165d
heipartho05 <heipartho72@gmail.com>
2022-12-08 22:36:53 +0600

    q15 added

---
+++ Part-02/Solution/q15.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+    int p1, p2;
+    int pick_num, guess_num;
+
+    printf("Player1 picks a number: ");
+    scanf("%d", &pick_num);
+
+    int num_of_guess = 1;
+    int total_guess = 3;
+
+    while (num_of_guess <= total_guess)
+    {
+        printf("Player2 guess any number: ");
+        scanf("%d", &guess_num);
+
+        if (guess_num > pick_num)
+        {
+            printf("You guess bigger number please enter less number \n");
+        }
+
+        else if (guess_num < pick_num)
+        {
+            printf("You guess less number enter any bigger number \n");
+        }
+
+        else if (guess_num == pick_num)
+        {
+            printf("Right, Player-2 wins! \n");
+            break;
+        }
+
+        printf("Wrong, %d chances left! \n", total_guess - num_of_guess);
+
+        num_of_guess += 1;
+    }
+
+    if (num_of_guess > 3)
+    {
+        printf("Game over! Player-01 wins! \n");
+    }
+}
