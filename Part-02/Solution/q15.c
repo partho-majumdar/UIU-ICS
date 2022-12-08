7a38ee88192dbf7967082382bdee320c3f77c6d2
heipartho05 <heipartho72@gmail.com>
2022-12-08 22:51:04 +0600

    Done ^_^

--- Part-02/Solution/q15.c
+++ Part-02/Solution/q15.c
@@ -4,18 +4,18 @@
 void main()
 {
     int p1, p2;
-    int pick_num, guess_num;
+    int pick_num = 10, guess_num = 15;
 
-    printf("Player1 picks a number: ");
-    scanf("%d", &pick_num);
+    // printf("Player-01 picks a number: ");
+    // scanf("%d", &pick_num);
 
     int num_of_guess = 1;
     int total_guess = 3;
 
     while (num_of_guess <= total_guess)
     {
-        printf("Player2 guess any number: ");
-        scanf("%d", &guess_num);
+        // printf("Player-02 guess any number: ");
+        // scanf("%d", &guess_num);
 
         if (guess_num > pick_num)
         {
@@ -29,7 +29,7 @@ void main()
 
         else if (guess_num == pick_num)
         {
-            printf("Right, Player-2 wins! \n");
+            printf("Right, Player-02 wins! \n");
             break;
         }
 
