c4ec8145c974f29cd22e4706afae4ff5de83a7c6
heipartho05 <heipartho72@gmail.com>
2022-12-13 22:29:56 +0600

    q2 added

---
+++ Part-04/Solution/Series with an upper limit/q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter value of n: ");
+    scanf("%d", &input_value);
+
+    for (int i = 1; i <= input_value; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d ", i);
+        }
+    }
+}
