c33ab9038a27c1bf1b66fd4d082fdab217a5d232
heipartho05 <heipartho72@gmail.com>
2022-12-23 22:19:39 +0600

    q1 update

--- Part-02/Solution/q1.c
+++ Part-02/Solution/q1.c
@@ -3,6 +3,7 @@
 void main()
 {
     float input_value;
+
     printf("Enter any number to check whether a number is positive or not: ");
     scanf("%f", &input_value);
 
@@ -10,8 +11,19 @@ void main()
     {
         printf("You entered %.2f and this number is negative", input_value);
     }
-    else
+
+    else if (input_value == 0)
     {
-        printf("You entered %.2f and this number is positive", input_value);
+        printf("You entered %.f and this number is positive", input_value);
+    }
+
+    else if (input_value == (int)(input_value))
+    {
+        printf("You entered %.f and this number is positive", input_value);
+    }
+
+    else if (input_value > 0)
+    {
+        printf("You enter ed %.2f and this number is positive", input_value);
     }
 }
