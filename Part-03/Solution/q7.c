b27348e73e3ee8652a891b1f10e6d07baa6345a2
heipartho05 <heipartho72@gmail.com>
2022-12-23 22:32:34 +0600

    q7 added

---
+++ Part-03/Solution/q7.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value = 5;
+
+    // scanf("%d", &input_value);
+
+    if (input_value == 1)
+    {
+        printf("Saturday");
+    }
+
+    else if (input_value == 2)
+    {
+        printf("Sunday");
+    }
+
+    else if (input_value == 3)
+    {
+        printf("Monday");
+    }
+
+    else if (input_value == 4)
+    {
+        printf("Tuesday");
+    }
+
+    else if (input_value == 5)
+    {
+        printf("Wednesday");
+    }
+
+    else if (input_value == 6)
+    {
+        printf("Thursday");
+    }
+
+    else if (input_value == 7)
+    {
+        printf("Friday");
+    }
+}
