a08dd24488e1889ff9544ddb9b9245c15de12517
heipartho05 <heipartho72@gmail.com>
2022-12-26 15:40:29 +0600

    q6 added

---
+++ Part-05-While-Loop/Solution/Digit extraction/q6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter any number: ");
+    scanf("%d", &input_value);
+
+    int temp_value = input_value;
+    int sum = 0;
+    while (input_value != 0)
+    {
+        int last_digit = input_value % 10;
+        sum += pow(last_digit, 3);
+        input_value = input_value / 10;
+    }
+
+    if (temp_value == sum)
+    {
+        printf("Armstrong");
+    }
+
+    else
+    {
+        printf("Not Armstrong");
+    }
+}
