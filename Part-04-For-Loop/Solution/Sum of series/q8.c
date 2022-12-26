91c7582a165c328fe24aa914e989a690254fee3b
heipartho05 <heipartho72@gmail.com>
2022-12-27 01:14:07 +0600

    q8 added

---
+++ Part-04-For-Loop/Solution/Sum of series/q8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter any value: ");
+    scanf("%d", &input_value);
+
+    int sum = 0;
+
+    for (int i = 1, j = 1; i <= input_value; i++, j++)
+    {
+        sum = sum + (j * j);
+    }
+
+    printf("Answer: %d", sum);
+}
