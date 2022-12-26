631fd17100823bad2f8108166bb393c890396a0b
heipartho05 <heipartho72@gmail.com>
2022-12-26 14:09:36 +0600

    q2 added

---
+++ Part-05-While-Loop/Solution/Digit extraction/q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value, last_digit;
+    printf("Enter any number: ");
+    scanf("%d", &input_value);
+
+    int sum = 0;
+    while (input_value != 0)
+    {
+        last_digit = input_value % 10;
+        sum = (sum * 10) + last_digit;
+        input_value = input_value / 10;
+    }
+
+    printf("%d", sum);
+}
