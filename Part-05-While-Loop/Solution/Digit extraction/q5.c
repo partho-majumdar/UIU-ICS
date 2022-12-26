d0afc9a62a5eddcc11307f25629cbe456003946f
heipartho05 <heipartho72@gmail.com>
2022-12-26 15:33:32 +0600

    q5 added

---
+++ Part-05-While-Loop/Solution/Digit extraction/q5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int input_number = 65237, first, last, swap, digit, divide;
+    // printf("Enter any number: ");
+    // scanf("%d", &input_number);
+
+    digit = log10(input_number);
+
+    divide = pow(10, digit);
+
+    first = input_number / divide;
+
+    input_number = input_number % divide;
+
+    last = input_number % 10;
+
+    input_number = input_number / 10;
+
+    swap = (last * divide) + (input_number * 10) + first;
+
+    printf("Answer: %d", swap);
+}
