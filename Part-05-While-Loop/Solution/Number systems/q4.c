43736ff10406fe86645bf54a3c0c85153507b8c5
heipartho05 <heipartho72@gmail.com>
2022-12-26 16:33:50 +0600

    q4 added

---
+++ Part-05-While-Loop/Solution/Number systems/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter octal value: ");
+    scanf("%d", &input_value);
+
+    int sum = 0, i = 0;
+    while (input_value != 0)
+    {
+        int last_digit = input_value % 10;
+        sum = sum + (last_digit * pow(8, i));
+        input_value = input_value / 10;
+        i++;
+    }
+
+    printf("Decimal: %d", sum);
+}
