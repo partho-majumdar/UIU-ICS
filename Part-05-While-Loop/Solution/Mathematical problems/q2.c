40ece66a79d91917b144025ec305cabdae1bc6cb
heipartho05 <heipartho72@gmail.com>
2022-12-26 13:11:14 +0600

    q2 added

---
+++ Part-05-While-Loop/Solution/Mathematical problems/q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value1, input_value2, remainder, gcd, lcm;
+
+    printf("Enter first value: ");
+    scanf("%d", &input_value1);
+
+    printf("Enter second value: ");
+    scanf("%d", &input_value2);
+
+    int temp_value1 = input_value1;
+    int temp_value2 = input_value2;
+
+    while (temp_value2 != 0)
+    {
+        remainder = temp_value1 % temp_value2;
+        temp_value1 = temp_value2;
+        temp_value2 = remainder;
+    }
+
+    gcd = temp_value1;
+    lcm = (input_value1 * input_value2) / gcd;
+
+    printf("LCM : %d", lcm);
+}
