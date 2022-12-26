16955a0ad729d62228714d2bd22ef8bef9e0bba9
heipartho05 <heipartho72@gmail.com>
2022-12-27 00:11:00 +0600

    q3 added

---
+++ Part-04-For-Loop/Solution/Series with no of terms/q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int input_value;
+    printf("Enter any number: ");
+    scanf("%d", &input_value);
+
+    for (int i = 1, j = 2; i <= input_value; i++, j += 2)
+    {
+        printf("%d ", j);
+    }
+}
