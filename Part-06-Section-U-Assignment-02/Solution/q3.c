ee8e69384b4f37f23b044cae2b0a5606a1bdbc3f
heipartho05 <heipartho72@gmail.com>
2022-12-29 15:36:41 +0600

    q3 added

---
+++ Part-06-Section-U-Assignment-02/Solution/q3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+{
+    int item_number, quantity;
+    float Total;
+
+    printf("Enter item number: ");
+    scanf("%d", &item_number);
+
+    printf("Enter amount of an item: ");
+    scanf("%d", &quantity);
+
+    switch (item_number)
+    {
+    case 1:
+        printf("Total: R$ %.2f", 4.00 * quantity);
+        break;
+
+    case 2:
+        printf("Total: R$ %.2f", 4.50 * quantity);
+        break;
+
+    case 3:
+        printf("Total: R$ %.2f", 5.00 * quantity);
+        break;
+
+    case 4:
+        printf("Total: R$ %.2f", 2.00 * quantity);
+        break;
+
+    case 5:
+        printf("Total: R$ %.2f", 1.50 * quantity);
+        break;
+    }
+}
