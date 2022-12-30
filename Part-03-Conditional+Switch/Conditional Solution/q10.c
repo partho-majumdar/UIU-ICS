11e660be7bb05e52e764b67474193ffc8ba03fa2
heipartho05 <heipartho72@gmail.com>
2022-12-30 14:18:18 +0600

    q10 added

---
+++ Part-03-Conditional+Switch/Conditional Solution/q10.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+    float basic_salary;
+    float hra, pba, gross_salary;
+
+    printf("Enter salary: ");
+    scanf("%f", &basic_salary);
+
+    if (basic_salary <= 10000)
+    {
+        hra = basic_salary * 0.2;
+        pba = basic_salary * 0.8;
+        gross_salary = basic_salary + hra + pba;
+        printf("Gross salary: %.2f", gross_salary);
+    }
+
+    else if (basic_salary <= 20000)
+    {
+        hra = basic_salary * 0.25;
+        pba = basic_salary * 0.9;
+        gross_salary = basic_salary + hra + pba;
+        printf("Gross salary: %.2f", gross_salary);
+    }
+
+    else if (basic_salary > 20000)
+    {
+        hra = basic_salary * 0.3;
+        pba = basic_salary * 0.95;
+        gross_salary = basic_salary + hra + pba;
+        printf("Gross salary: %.2f", gross_salary);
+    }
+}
