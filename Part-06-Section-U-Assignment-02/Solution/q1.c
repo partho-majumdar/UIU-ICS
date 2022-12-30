23fa4318bd71a9b547b6d30ae562155146eb8cf4
heipartho05 <heipartho72@gmail.com>
2022-12-30 15:14:03 +0600

    q1 added

--- Part-06-Section-U-Assignment-02/Solution/q1.c
+++ Part-06-Section-U-Assignment-02/Solution/q1.c
@@ -2,30 +2,30 @@
 
 void main()
 {
-    int input_num, first_num, large_num, small_num;
+    int input_num, first_num, other_num;
 
-    printf("Enter how many number you want to print: ");
+    printf("Enter how many number you want to take: ");
     scanf("%d", &input_num);
 
     printf("Enter first number: ");
     scanf("%d", &first_num);
 
-    large_num = first_num;
-    small_num = first_num;
+    int large_num = first_num;
+    int small_num = first_num;
 
     for (int i = 1; i < input_num; i++)
     {
         printf("Enter another number: ");
-        scanf("%d", &first_num);
+        scanf("%d", &other_num);
 
-        if (first_num > large_num)
+        if (other_num > large_num)
         {
-            large_num = first_num;
+            large_num = other_num;
         }
 
-        if (first_num < small_num)
+        if (other_num < small_num) 
         {
-            small_num = first_num;
+            small_num = other_num;
         }
     }
 
